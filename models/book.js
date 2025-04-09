d13e629e0f096891ce437891cc554dd034378b52
Ananya Biswas <153214834+ananyab1909@users.noreply.github.com>
2025-04-09 16:44:03 +0530

    added book.js

---
+++ models/book.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const BookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  stock: {
+    type: Number,
+    required: true,
+    default: 10,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 5,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Book = mongoose.model("book", BookSchema);
